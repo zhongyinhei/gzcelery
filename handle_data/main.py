ec8fd097994c3d95f610cc78d9caea079d142f7a
wangyao <13564118478@163.com>
2019-08-09 15:15:50 +0800

    稍作修改

--- handle_data/main.py
+++ handle_data/main.py
@@ -6,6 +6,6 @@ def handle_data(data_str):
     # res = chain(to_product.s(data_str), to_analysis.s(), to_consume.s())()
 
     # 插入一条pickle后的数据，返回记录的id res1
-    # res = to_create.apply_async(args=[data_str], retry=True, queue='to_create', immutable=True)
-    res = to_create(data_str)
-    print(res)
+    res = to_create.apply_async(args=[data_str], retry=True, queue='to_create', immutable=True)
+    # res = to_create(data_str)
+    # print(res)
